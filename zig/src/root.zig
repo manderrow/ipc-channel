5b1bd454ae2ff17ef650cf65c74ab5ffbe800b5d
Michael Pfaff <michael@pfaff.dev>
2025-04-15 18:01:05 -0400

    Add a few exports

--- zig/src/root.zig
+++ zig/src/root.zig
@@ -1,27 +1,10 @@
-const std = @import("std");
-
 pub const os = @import("os.zig");
 
 test {
     _ = os;
 }
 
-const alloc = std.heap.smp_allocator;
-
-export fn ipc_channel_os_ipc_sender_send(
-    self: *os.Sender,
-    data: [*]const u8,
-    data_len: usize,
-    ports: [*]os.Channel,
-    ports_len: usize,
-    shared_memory_regions: [*]os.SharedMemory,
-    shared_memory_regions_len: usize,
-) u16 {
-    self.send(
-        alloc,
-        data[0..data_len],
-        ports[0..ports_len],
-        shared_memory_regions[0..shared_memory_regions_len],
-    ) catch |e| return @intFromError(e);
-    return 0;
+comptime {
+    // export the C API
+    _ = @import("c.zig");
 }
