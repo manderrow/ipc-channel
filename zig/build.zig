8340927a4494f0b48b39f49f802ee178f2b48641
Michael Pfaff <michael@pfaff.dev>
2025-04-15 18:01:05 -0400

    Add a few exports

--- zig/build.zig
+++ zig/build.zig
@@ -8,24 +8,28 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = false,
     });
 
-    const lib = b.addLibrary(.{
+    b.installArtifact(b.addLibrary(.{
         .linkage = .static,
         .name = "ipc_channel",
         .root_module = lib_mod,
-    });
+    }));
 
-    b.installArtifact(lib);
+    b.installArtifact(b.addLibrary(.{
+        .linkage = .dynamic,
+        .name = "ipc_channel",
+        .root_module = lib_mod,
+    }));
 
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
 
-    b.installArtifact(lib_unit_tests);
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&b.addInstallArtifact(lib_unit_tests, .{}).step);
     test_step.dependOn(&run_lib_unit_tests.step);
 }
---
+++ zig/src/c.zig
@@ -0,0 +1,89 @@
+const std = @import("std");
+
+const os = @import("os.zig");
+
+const alloc = std.heap.smp_allocator;
+
+pub fn Slice(comptime T: type) type {
+    return extern struct {
+        ptr: [*]T,
+        len: usize,
+
+        pub fn from(items: []T) @This() {
+            return .{ .ptr = items.ptr, .len = items.len };
+        }
+
+        pub fn slice(self: @This()) []T {
+            return self.ptr[0..self.len];
+        }
+    };
+}
+
+pub const Message = extern struct {
+    data: Slice(u8),
+    channels: Slice(os.OpaqueChannel),
+    shared_memory_regions: Slice(os.SharedMemory),
+};
+
+pub export fn ipc_channel_create(
+    sender: *os.Sender,
+    receiver: *os.Receiver,
+) u16 {
+    const result = os.channel() catch |e| return @intFromError(e);
+    sender.* = result.sd;
+    receiver.* = result.rc;
+    return 0;
+}
+
+pub export fn ipc_channel_send(
+    self: *os.Sender,
+    data: [*]const u8,
+    data_len: usize,
+    ports: [*]os.Channel,
+    ports_len: usize,
+    shared_memory_regions: [*]os.SharedMemory,
+    shared_memory_regions_len: usize,
+) u16 {
+    self.send(
+        alloc,
+        data[0..data_len],
+        ports[0..ports_len],
+        shared_memory_regions[0..shared_memory_regions_len],
+    ) catch |e| return @intFromError(e);
+    return 0;
+}
+
+pub export fn ipc_channel_recv(
+    self: *os.Receiver,
+    message: *Message,
+) u16 {
+    const msg = self.recv(alloc) catch |e| return @intFromError(e);
+    message.data = .from(msg.data);
+    message.channels = .from(msg.channels);
+    message.shared_memory_regions = .from(msg.shared_memory_regions);
+    return 0;
+}
+
+pub const FreeMessageFlags = packed struct(u32) {
+    /// Whether to free the data array.
+    free_data: bool,
+    /// Whether to free the channels array. This will not free the contents.
+    free_channels: bool,
+    /// Whether to free the shared memory regions array. This will not free the contents.
+    free_shared_memory_regions: bool,
+};
+
+pub export fn ipc_channel_free_message(
+    message: *Message,
+    flags: FreeMessageFlags,
+) u16 {
+    if (flags.free_data) {
+        alloc.free(message.data.slice());
+    }
+    if (flags.free_channels) {
+        alloc.free(message.channels.slice());
+    }
+    if (flags.free_shared_memory_regions) {
+        alloc.free(message.shared_memory_regions.slice());
+    }
+}
